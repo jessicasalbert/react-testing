4d4d5ea11a48fd8708ba3a7e1fffaa2b6b3c7e68
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-01 17:51:18 -0500

    Configure enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow}  from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+
+// Shallow rendering - renders components only one level deep 
+// Keeps things clean
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App/>)
+  expect(wrapper).toBeTruthy
 });
